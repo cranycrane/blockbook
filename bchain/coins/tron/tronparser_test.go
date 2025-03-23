b7e14f25a36d767c28cd95893e13f5112ebee962
Vacranycrane <[xjerab28@stud.fit.vutbr.cz>
2025-03-23 17:42:01 +0000

    add tron parser tests GetTokenTransfersFromTx

--- bchain/coins/tron/tronparser_test.go
+++ bchain/coins/tron/tronparser_test.go
@@ -108,7 +108,8 @@ func TestTronParser_GetAddressesFromAddrDesc(t *testing.T) {
 	}
 }
 
-func TestTronParser_EthereumTypeGetTokenTransfersFromTx(t *testing.T) {
+// Receipt != nil so we are testing getting transfers from og
+func TestTronParser_EthereumTypeGetTokenTransfersFromLog(t *testing.T) {
 	parser := NewTronParser(1, false)
 
 	tests := []struct {
@@ -225,6 +226,84 @@ func TestTronParser_EthereumTypeGetTokenTransfersFromTx(t *testing.T) {
 							Value: *big.NewInt(1),
 						},
 					},
+				},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			transfers, err := parser.EthereumTypeGetTokenTransfersFromTx(tt.tx)
+			if err != nil {
+				t.Fatalf("unexpected error: %v", err)
+			}
+			if len(tt.expected) != len(transfers) {
+				t.Fatalf("expected %d transfers, got %d", len(tt.expected), len(transfers))
+			}
+
+			for i := range tt.expected {
+				if tt.expected[i].Contract != transfers[i].Contract ||
+					tt.expected[i].Type != transfers[i].Type ||
+					tt.expected[i].From != transfers[i].From ||
+					tt.expected[i].To != transfers[i].To ||
+					tt.expected[i].Value.Cmp(&transfers[i].Value) != 0 {
+					t.Errorf("transfer %d mismatch:\ngot  %+v\nwant %+v", i, transfers[i], tt.expected[i])
+				}
+			}
+
+		})
+	}
+}
+
+func TestTronParser_EthereumTypeGetTokenTransfersFromTx(t *testing.T) {
+	parser := NewTronParser(1, false)
+
+	tests := []struct {
+		name     string
+		tx       *bchain.Tx
+		expected bchain.TokenTransfers
+	}{
+		{
+			name: "TRC20 transfer",
+			tx: &bchain.Tx{
+				Txid: "0xtesttxid",
+				CoinSpecificData: bchain.EthereumSpecificData{
+					Tx: &bchain.RpcTransaction{
+						From:    "0xc88bb5a4636463d7eb2af02ccabb8b790fb200a9",
+						To:      "0xa614f803b6fd780986a42c78ec9c7f77e6ded13c",                                                                                                 // kontrakt
+						Payload: "0xa9059cbb0000000000000000000000418da98894069283ddf2379e0b27bfea76fc9b73990000000000000000000000000000000000000000000000000000000022eda680", // transfer(address,uint256)
+					},
+				},
+			},
+			expected: bchain.TokenTransfers{
+				{
+					Type:     bchain.FungibleToken,
+					Contract: "TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t", // Base58
+					From:     "TUFbWcZzvLy2LbxkxFAraojZRTB8vewjsz", // Base58
+					To:       "TNtFNW4EoQJanSczatPpU2kETN3WbVFVHR", // Base58
+					Value:    *big.NewInt(586000000),               // doplníme
+				},
+			},
+		},
+		{
+			name: "TRC721 transfer",
+			tx: &bchain.Tx{
+				Txid: "0x49ced31cd0fd6d8e1126775f53ade165fe7ca43e9cc968d64a9ce1aff597423c",
+				CoinSpecificData: bchain.EthereumSpecificData{
+					Tx: &bchain.RpcTransaction{
+						From:    "0x34627862d50389c8d7a1ab5ef074b84ab4ddb9e9",
+						To:      "0x0b17822171ee88e98d4a61029f97c9f8edc15fcd",
+						Payload: "0x23b872dd00000000000000000000000034627862d50389c8d7a1ab5ef074b84ab4ddb9e90000000000000000000000000cecca0e53477d2b6c562ab68c3452fc99f7817e000000000000000000000000000000000000000000000000000000000000067f", // volitelné – není potřeba u log-based NFT přenosu
+					},
+				},
+			},
+			expected: bchain.TokenTransfers{
+				{
+					Type:     bchain.NonFungibleToken,
+					Contract: "TAyrbZCme4jVBnHnALvoKbE6ewLd2VGD77",
+					From:     "TEkC6sH3rPjwXzXm58p9dRVVMHiz2wTcub",
+					To:       "TB9YmmXyQuhZ4dvG4T2EAzeksVme6RSvWA",
+					Value:    *big.NewInt(1663),
 				},
 			},
 		},
